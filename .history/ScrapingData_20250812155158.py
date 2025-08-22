499d732de91d05ad98e40af4a4267554ae840c21
Admin <HAM0909>
2025-08-22 23:19:26 +0100

    Initial commit with project files

--- .history/ScrapingData_20250812155158.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = 'https://books.toscrape.com/'
-
-response = request.get(url)
--- .history/table_livre_20250819103111.py
+++
@@ -1,7 +0,0 @@
-
-    print("Table 'livres' créée avec succès !")
-except Exception as e:
-    print("Erreur lors de la création de la table :", e)
-
-cursor.close()
-conn.close()
